5842c30dafe8b433f9574ec43bc6e00635fd7ca7
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 12:56:03 +0300

    Create app.php

---
+++ src/Resources/lang/tr/app.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'admin' => [
+        'system' => [
+            'ziraatPayment' => 'ziraat Payment',
+            'title' => 'Başlık',
+            'description' => 'Açıklama',
+            'status' => 'Durum',
+			'sandbox' => 'Test Durumu',
+            'client-id' => 'Client ID',
+            'api-user' => 'API User',
+            'store-key' => 'Store Key',
+            'api-user-password' => '3D Key',
+			//'allowed-installments' => 'Taksit Sayıları',
+            'sort' => 'Sıralama Sırası',
+			'info' => 'İnternetten alışveriş deneyimini hem alıcılar hem de satıcılar için kolaylaştıran bir finansal teknolojiler şirketidir.',
+        ],
+
+    ],'shop' => [
+        'went-wrong' => 'Something went wrong !',
+        'payment-cancelled' => 'ziraat Payment has been cancelled',
+    ]
+];  
