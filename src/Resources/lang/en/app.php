76a4915440bda0ad31063c1464c733b29573c705
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 12:56:23 +0300

    Update app.php

--- src/Resources/lang/en/app.php
+++ src/Resources/lang/en/app.php
@@ -1,1 +1,25 @@
+<?php
+
+return [
+    'admin' => [
+        'system' => [
+            'ziraatPayment' => 'ziraat Payment',
+            'title' => 'Title',
+            'description' => 'Description',
+            'status' => 'Status',
+			'sandbox' => 'Sandbox',
+            'client-id' => 'Client ID',
+            'api-user' => 'API User',
+            'store-key' => 'Store Key',
+            'api-user-password' => '3D Key',
+			//'allowed-installments' => 'Allowed Installments',
+            'sort' => 'Sort',
+			'info' => 'is a financial technology company facilitating online shopping experience for both shoppers and sellers.',
+        ],
+
+    ],'shop' => [
+        'went-wrong' => 'Something went wrong !',
+        'payment-cancelled' => 'ziraat Payment has been cancelled',
+    ]
+];  
 
