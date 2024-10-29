1996aad8ae8073706668a514de13fdb840adb66f
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 12:45:25 +0300

    Create paymentmethods.php

---
+++ src/Config/paymentmethods.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'ziraat' => [
+        'code' => 'ziraat',
+        'title' => 'ziraat',
+        'description' => 'ziraat Payment Gateway',
+        'image' => asset('/vendor/aghaeian/ziraat/assets/images/ziraat.png'),
+        'class' => 'aghaeian\ziraat\Payment\ziraat',
+        'active' => true,
+        'sort'=> 5,
+    ]
+];
