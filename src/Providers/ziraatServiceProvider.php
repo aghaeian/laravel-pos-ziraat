07a810df0f8919b621ea3f41a8ae4fa239f8eae5
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 12:49:31 +0300

    Create ziraatServiceProvider.php

---
+++ src/Providers/ziraatServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+ 
+namespace aghaeian\ziraat\Providers;
+
+use Illuminate\Support\ServiceProvider;
+ 
+/**
+ *  ziraat service provider
+ *
+ * @author  aghaeian
+ */
+class ziraatServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    
+   
+    public function boot()
+    {
+		
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+
+        $this->loadTranslationsFrom(__DIR__ . '/../Resources/lang', 'ziraat');
+
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'ziraat');
+
+    }
+ 
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {   
+        $this->registerConfig();
+    }
+
+    /**
+     * Register package config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {   
+        //this will merge payment method
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/Config/paymentmethods.php', 'payment_methods'
+        );
+
+        // add menu inside configuration  
+        $this->mergeConfigFrom(
+            dirname(__DIR__) . '/Config/system.php', 'core'   
+        );
+
+    }
+}
