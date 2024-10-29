53753ad22e8012122eeb67ec3d149fe679da62f2
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 12:58:01 +0300

    Create ziraat-payment-callback.blade.php

---
+++ src/Resources/views/ziraat-payment-callback.blade.php
@@ -0,0 +1,18 @@
+<x-shop::layouts.account>
+    <!-- Title of the page -->
+    <x-slot:title>
+        @lang('ziraat::app.admin.system.ziraatPayment')
+    </x-slot:title>
+
+    <div class="flex justify-between items-center">
+        <h2 class="text-[26px] font-medium">
+            @section('page_title')
+				{{ __('ziraat Checkout....') }}
+			@stop
+
+			@push('scripts')
+				<?php echo $paymentcontent_msg; ?>
+			@endpush
+        </h2>
+    </div>
+</x-shop::layouts.account>
