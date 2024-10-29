11d317f59d5081e6052bc0ce1c1e86d2b1afd763
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-29 13:11:19 +0300

    Update ziraat-payment-callback.blade.php

--- src/Resources/views/ziraat-payment-callback.blade.php
+++ src/Resources/views/ziraat-payment-callback.blade.php
@@ -7,12 +7,12 @@
     <div class="flex justify-between items-center">
         <h2 class="text-[26px] font-medium">
             @section('page_title')
-				{{ __('ziraat Checkout....') }}
-			@stop
+                {{ __('ziraat Checkout....') }}
+            @stop
 
-			@push('scripts')
-				<?php echo $paymentcontent_msg; ?>
-			@endpush
+            @push('scripts')
+                <?php echo $paymentcontent_msg; ?>
+            @endpush
         </h2>
     </div>
 </x-shop::layouts.account>
